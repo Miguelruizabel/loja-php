d2673099c7753f1e3f76abbd6677cc38e9f99fd3
Miguelruizabel <miguelangeloruiz09@gmail.com>
2025-05-28 12:45:54 -0300

    Update dbconnect.php

--- projetoPHP/database/dbconnect.php
+++ projetoPHP/database/dbconnect.php
@@ -1,14 +1,14 @@
 
 <?php
-// Configurações do banco de dados
+
 $host = 'localhost';
-$db   = 'loja_virtual'; // Altere para o nome do seu banco
+$db   = 'loja_virtual'; 
 $user = 'root';
-$pass = ''; // Altere se sua senha for diferente
+$pass = ''; 
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-    // Ativa o modo de erros do PDO
+    
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Erro na conexão com o banco de dados: " . $e->getMessage());
