b43d9acb67d23e69a0ab6e27081f8d6cfc241f26
Miguelruizabel <miguelangeloruiz09@gmail.com>
2025-05-28 13:56:36 -0300

    Update CategoriaService.php

--- projetoPHP/service/CategoriaService.php
+++ projetoPHP/service/CategoriaService.php
@@ -1,5 +1,5 @@
-
 <?php
+
 
 require_once __DIR__ . '/../database/dbconnect.php';
 require_once __DIR__ . '/../model/Categoria.php';
@@ -22,28 +22,5 @@ class CategoriaService
             $categorias[] = new Categoria($row['id'], $row['nome']);
         }
         return $categorias;
-    }
-
-    public function salvarCategoria($nome)
-    {
-        $stmt = $this->pdo->prepare("INSERT INTO categorias (nome) VALUES (:nome)");
-        return $stmt->execute(['nome' => $nome]);
-    }
-
-    public function excluirCategoria($id)
-    {
-        $stmt = $this->pdo->prepare("DELETE FROM categorias WHERE id = :id");
-        return $stmt->execute(['id' => $id]);
-    }
-
-    public function buscarCategoriaPorId($id)
-    {
-        $stmt = $this->pdo->prepare("SELECT * FROM categorias WHERE id = :id");
-        $stmt->execute(['id' => $id]);
-        $row = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($row) {
-            return new Categoria($row['id'], $row['nome']);
-        }
-        return null;
     }
 }
